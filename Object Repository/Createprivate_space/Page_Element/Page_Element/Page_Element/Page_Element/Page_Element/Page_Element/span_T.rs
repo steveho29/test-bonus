e210b85ccae8e82157479c5c593d0542d9f6b0a6
MKforIT1510 <75960875+MKforIT1510@users.noreply.github.com>
2022-01-15 23:28:57 +0700

    CreateGroupchatPublic

---
+++ Object Repository/Createprivate_space/Page_Element/Page_Element/Page_Element/Page_Element/Page_Element/Page_Element/span_T.rs
@@ -0,0 +1,96 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>span_T</name>
+   <tag></tag>
+   <elementGuidId>56b6909e-449b-4054-a4d8-22b595e85322</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//section[@id='matrixchat']/div/div[2]/div/ul/div[2]/div/li[3]/div/div[2]/div/span</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>li.mx_SpaceItem.mx_SpaceItem_narrow.collapsed.hasSubSpaces > div.mx_AccessibleButton.mx_SpaceButton.mx_SpaceButton_narrow > div.mx_SpaceButton_selectionWrapper > div.mx_SpaceButton_avatarWrapper > span.mx_BaseAvatar.mx_RoomAvatar_isSpaceRoom</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>span</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>mx_BaseAvatar mx_RoomAvatar_isSpaceRoom</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>role</name>
+      <type>Main</type>
+      <value>presentation</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>T</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;matrixchat&quot;)/div[@class=&quot;mx_MatrixChat_wrapper&quot;]/div[@class=&quot;mx_MatrixChat&quot;]/div[@class=&quot;mx_LeftPanel_wrapper&quot;]/ul[@class=&quot;mx_SpacePanel collapsed&quot;]/div[@class=&quot;mx_AutoHideScrollbar mx_SpacePanel_spaceTreeWrapper&quot;]/div[@class=&quot;mx_SpaceTreeLevel&quot;]/li[@class=&quot;mx_SpaceItem mx_SpaceItem_narrow collapsed hasSubSpaces&quot;]/div[@class=&quot;mx_AccessibleButton mx_SpaceButton mx_SpaceButton_narrow&quot;]/div[@class=&quot;mx_SpaceButton_selectionWrapper&quot;]/div[@class=&quot;mx_SpaceButton_avatarWrapper&quot;]/span[@class=&quot;mx_BaseAvatar mx_RoomAvatar_isSpaceRoom&quot;]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//section[@id='matrixchat']/div/div[2]/div/ul/div[2]/div/li[3]/div/div[2]/div/span</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Verify'])[1]/following::span[5]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Later'])[1]/following::span[5]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Ctrl K'])[1]/preceding::span[6]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Home'])[1]/preceding::span[6]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[2]/div/span</value>
+   </webElementXpaths>
+</WebElementEntity>
